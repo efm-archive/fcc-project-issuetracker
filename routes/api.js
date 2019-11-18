4f9ef978657c53074348ed06f22321fe7cfc5476
Emil Félix Muerrle <gitcommit@chaiya.es>
2019-11-18 08:55:19 +0100

    DB running

--- routes/api.js
+++ routes/api.js
@@ -1,41 +1,62 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
 'use strict';
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
+const mongoose = require('mongoose');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+require('dotenv').config();
 
-module.exports = function (app) {
+const CONNECTION_STRING = process.env.MONGO; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+const Schema = mongoose.Schema;
+
+const issueSchema = new Schema({
+  username: String,
+  exercises: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Exercise'
+    }
+  ]
+});
+
+const Issue = mongoose.model('Issue', issueSchema);
+
+module.exports = function(app) {
+  mongoose.connect(CONNECTION_STRING, (err, db) => {
+    if (err) {
+      console.log('Database error: ' + err);
+    } else {
+      console.log('Successful database connection');
+    }
+  });
+
+  app
+    .route('/api/issues/:project')
+
+    .get(function(req, res) {
       var project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
+
+    .post(function(req, res) {
       var project = req.params.project;
-      
+      console.log('req.body :', req.body);
     })
-    
-    .put(function (req, res){
+
+    .put(function(req, res) {
       var project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function(req, res) {
       var project = req.params.project;
-      
     });
-    
 };
