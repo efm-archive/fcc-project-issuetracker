9b29b286b115f7cd34944eff167c4c1b42f429dc
Emil Félix Muerrle <gitcommit@chaiya.es>
2019-11-19 18:05:14 +0100

    CP2 - POST & GET

--- routes/api.js
+++ routes/api.js
@@ -22,11 +22,14 @@ const Schema = mongoose.Schema;
 
 const issueSchema = new Schema({
   project: { type: String, required: true },
-  title: { type: String, required: true },
-  text: { type: String, required: true },
-  createdBy: { type: String, required: true },
-  assignedTo: String,
-  statusText: String
+  issue_title: { type: String, required: true },
+  issue_text: { type: String, required: true },
+  created_by: { type: String, required: true },
+  assigned_to: String,
+  status_text: String,
+  created_on: { type: Date, default: Date.now },
+  updated_on: { type: Date, default: Date.now },
+  open: Boolean
 });
 
 const Issue = mongoose.model('Issue', issueSchema);
@@ -43,42 +46,63 @@ module.exports = function(app) {
   app
     .route('/api/issues/:project')
 
-    .get(async function(req, res) {
+    .get(async function(req, res, next) {
       var project = req.params.project;
-      // console.log('req.body :', req.body);
-      const issues = await Issue.find({}, (err, issues) => {
-        const result = [];
-        issues.forEach(issue => {
-          console.log('issue :', issue);
-        });
-        res.send(issues);
-      });
+
+      //set the default filter, and include the project
+      const filters = {
+        project: project
+      };
+
+      // loop over the req.query object
+      for (const key in req.query) {
+        // add each filter to the filters object
+        filters[key] = req.query[key];
+      }
+      //find the issues for the given project
+      const issues = await Issue.find({ ...filters }, (err, issues) => {});
+
+      res.status(200).send(issues);
+      return next();
     })
 
-    .post(async function(req, res) {
+    .post(async function(req, res, next) {
       var project = req.params.project;
       // console.log('req.body :', req.body);
+
+      // check if all required fields are present
+      const { issue_title, issue_text, created_by } = req.body;
+      if (!issue_title || !issue_text || !created_by) {
+        res
+          .status(400)
+          .send('Title, Text and Created by fields must be filled in!');
+        return next();
+      }
 
       // create a new Issue for the passed in project with the req.body data
       const newIssue = new Issue({
         project: project,
-        title: req.body.issue_title,
-        text: req.body.issue_text,
-        createdBy: req.body.created_by,
-        assignedTo: req.body.assigned_to,
-        statusText: req.body.status_text
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || '',
+        open: true
       });
-      // console.log('newIssue :', newIssue);
 
       // save the Issue to the database
-      newIssue.save((err, doc) => {});
+      await newIssue.save((err, doc) => {
+        // return status and the new issue
+        res.status(200).send(doc);
+        return next();
+      });
     })
 
-    .put(function(req, res) {
+    .put(async function(req, res, next) {
       var project = req.params.project;
     })
 
-    .delete(function(req, res) {
+    .delete(async function(req, res, next) {
       var project = req.params.project;
     });
 };
